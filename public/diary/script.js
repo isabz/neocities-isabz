0f5694b9368dc0524446550604fd63d92ebae455
isabz <59798329+isabz@users.noreply.github.com>
2022-08-19 22:47:29 +0100

    new diary entry

--- public/diary/script.js
+++ public/diary/script.js
@@ -34,6 +34,7 @@ let postsArray = [
 //[ "posts/2020-11-10-Special-Characters-Example.html", encodeURI( 'Spéci@l "Character\'s" Examp|e' ) ]
 //[ "posts/2020-11-10-My-Third-Post-Example.html" ],
 //[ "posts/2020-11-10-My-Second-Post-Example.html" ],
+["posts/2022-08-19-Rabbit-hole.html"],
 ["posts/2022-05-21-Summer.html"],
 ["posts/2022-04-22-Back-to-uni.html"],
 ["posts/2022-04-06-Easter-Break.html"]];
